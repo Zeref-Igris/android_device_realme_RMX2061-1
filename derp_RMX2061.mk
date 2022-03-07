5fb08060c0a44f6745b90f888ba59f67fdc10db3
NurKeinNeid <mralexman3000@gmail.com>
2022-03-07 15:02:49 +0000

    RMX2061: Enable Quick-tap

--- derp_RMX2061.mk
+++ derp_RMX2061.mk
@@ -14,6 +14,7 @@ WITH_GMS := true
 # Official-ify
 DERP_BUILDTYPE := Official
 USE_LEGACY_BOOTANIMATION := true
+TARGET_SUPPORTS_QUICK_TAP := true
 
 # Inherit some common stuff
 $(call inherit-product, vendor/derp/config/common_full_phone.mk)
