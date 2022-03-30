29c3e8908766fa1ab2b9b3380477582fd763e781
Zeref Igris <igriszeref.002@gmail.com>
2022-03-30 09:21:54 +0530

    Bringup for Awaken
    Signed-off-by: Zeref Igris <igriszeref.002@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/derp_RMX2061.mk
+    $(LOCAL_DIR)/awaken_RMX2061.mk
 
 COMMON_LUNCH_CHOICES := \
-    derp_RMX2061-user \
-    derp_RMX2061-userdebug \
-    derp_RMX2061-eng
+    awaken_RMX2061-user \
+    awaken_RMX2061-userdebug \
+    awaken_RMX2061-eng
